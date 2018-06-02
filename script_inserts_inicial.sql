93053a51766c241d8cbc36b29c4caf24a2704183
gianfagotti <gian97numero1@hotmail.com>
2018-06-02 20:30:02 -0300

    Subo DER y arreglos del script_inserts

--- script_inserts_inicial.sql
+++ script_inserts_inicial.sql
@@ -3,7 +3,7 @@ INSERT INTO FAGD.Hotel ([hotel_ciudad], [hotel_calle], [hotel_nroCalle], [hotel_
 	FROM [GD1C2018].[gd_esquema].[Maestra]
 
 INSERT INTO FAGD.Regimen ([regimen_descripcion],[regimen_precioBase])
-		SELECT DISTINCT ([Regimen_Descripcion],[Regimen_Precio])
+		SELECT DISTINCT [Regimen_Descripcion],[Regimen_Precio]
 		FROM [GD1C2018].[gd_esquema].[Maestra]
 
 INSERT INTO FAGD.Consumible ([consumible_codigo],[consumible_descripcion],[consumible_precio])
@@ -20,23 +20,21 @@ INSERT INTO FAGD.Cliente ([cliente_nroDocumento],[cliente_apellido],[cliente_nom
 INSERT INTO FAGD.Reserva ([reserva_codigo],[reserva_fechaInicio],[reserva_cantNoches],[reserva_codigoRegimen],[reserva_clienteNroDocumento],[reserva_codigoHotel],[reserva_nroHabitacion])
 		--[reserva_nombreUsuario],--
 		
-		SELECT DISTINCT ([Reserva_Codigo],[Reserva_Fecha_Inicio],[Reserva_Cant_Noches],
+		SELECT DISTINCT [Reserva_Codigo],[Reserva_Fecha_Inicio],[Reserva_Cant_Noches],
 		[(SELECT regimen_codigo FROM FAGD.Regimen, gd_esquema.Maestra WHERE regimen_descripcion = Regimen_Descripcion)],
 		[Cliente_Pasaporte_Nro],
-		[(SELECT hotel_codigo FROM FAGD.Hotel, gd_esquema.Maestra WHERE hotel_calle = Hotel_Calle and hotel_nroCalle = Hotel_Nro_Calle)],
-		--[(SELECT habitacion_codigo FROM FAGD.Habitacion, gd_esquema.Maestra WHERE NO ME DA EL TIEMPO PENSAR ESTE SELECT)]
-
-
+		[(SELECT hotel_codigo FROM FAGD.Hotel, gd_esquema.Maestra WHERE hotel_calle = Hotel_Calle and hotel_nroCalle = Hotel_Nro_Calle)]
+		--[(SELECT habitacion_codigo FROM FAGD.Habitacion, gd_esquema.Maestra WHERE NO ME DA EL TIEMPO PENSAR ESTE SELECT)]--
 		FROM [GD1C2018].[gd_esquema].[Maestra]
 GO
 -------------------------------- ROLES Y FUNCIONALIDADES INICIALES --------------------------------- 
-INSERT INTO FAGD.Rol (rol_codigo,rol_nombre,rol_estado)	
-		values (1,'administrador',1),(2,'recepcionista',1),(3,'cliente',1)
+INSERT INTO FAGD.Rol (rol_nombre,rol_estado)	
+		values ('administrador',1),('recepcionista',1),('cliente',1)
 GO
 
-INSERT INTO FAGD.Funcionalidad(funcionalidad_codigo,funcionalidad_cliente)
-		values (1,'ABM_Reservas'),(2,'ABM_Estadias'),(3,'ABM_Clientes'),(4,'ABM_Consumibles'),(5,'ABM_Roles'),(6,'ABM_Hoteles'),
-		       (7,'ABM_Habitaciones'),(8,'ABM_Usuarios'),(9,'ABM_Regimenes'),(10,'Estadisticas')
+INSERT INTO FAGD.Funcionalidad(funcionalidad_detalle)
+		values ('ABM_Reservas'),('ABM_Estadias'),('ABM_Clientes'),('ABM_Consumibles'),('ABM_Roles'),('ABM_Hoteles'),
+		       ('ABM_Habitaciones'),('ABM_Usuarios'),('ABM_Regimenes'),('Estadisticas')
 GO
 
 INSERT INTO FAGD.RolXFuncionalidad(rol_codigo,funcionalidad_codigo)
