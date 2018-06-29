bee7fbca5b357f5853ca3fad0fa3e658f6c873f6
tgullon <tomasg.97@hotmail.com>
2018-06-29 00:21:56 -0300

    Ajustes en abmRegistrarConsumible, formularios y codigo urgente ver cambios para el modelo DER

--- FrbaHotel/RegistrarConsumible/FrmAgregarConsumibles.Designer.cs
+++ FrbaHotel/RegistrarConsumible/FrmAgregarConsumibles.Designer.cs
@@ -28,14 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.CmbDescripcion = new System.Windows.Forms.ComboBox();
             this.LblRegistrarConsumibles = new System.Windows.Forms.Label();
-            this.LblDescripcion = new System.Windows.Forms.Label();
-            this.LblTotal = new System.Windows.Forms.Label();
             this.LblPrecioIndividual = new System.Windows.Forms.Label();
             this.LblCantidad = new System.Windows.Forms.Label();
             this.TxtPrecioIndividual = new System.Windows.Forms.TextBox();
-            this.TxtTotal = new System.Windows.Forms.TextBox();
             this.TxtCantidad = new System.Windows.Forms.TextBox();
             this.BtnAgregarConsumible = new System.Windows.Forms.Button();
             this.BtnModificarConsumible = new System.Windows.Forms.Button();
@@ -48,27 +44,18 @@
             this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BtnGuardarCambios = new System.Windows.Forms.Button();
             this.BtnVolver = new System.Windows.Forms.Button();
-            this.LblCodigoConsumible = new System.Windows.Forms.Label();
-            this.TxtCodigoConsumible = new System.Windows.Forms.TextBox();
             this.lblAllInclusive = new System.Windows.Forms.Label();
             this.cboInclusive = new System.Windows.Forms.CheckBox();
+            this.LblCodigoEstadia = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.txtCodigoEst = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigoConsu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // CmbDescripcion
-            // 
-            this.CmbDescripcion.AutoCompleteCustomSource.AddRange(new string[] {
-            "Papas Fritas Lays 60 grs.",
-            "Porrón cerveza Quilmes 340 ml.",
-            "Botella de agua Villavicencio 500 ml.",
-            "Champagne Chandon Extra Brut 500 ml.",
-            "Alfajor Fantoche",
-            ""});
-            this.CmbDescripcion.FormattingEnabled = true;
-            this.CmbDescripcion.Location = new System.Drawing.Point(166, 86);
-            this.CmbDescripcion.Name = "CmbDescripcion";
-            this.CmbDescripcion.Size = new System.Drawing.Size(121, 21);
-            this.CmbDescripcion.TabIndex = 0;
             // 
             // LblRegistrarConsumibles
             // 
@@ -83,33 +70,11 @@
             this.LblRegistrarConsumibles.Text = "Registro De Consumibles:";
             this.LblRegistrarConsumibles.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // LblDescripcion
-            // 
-            this.LblDescripcion.AutoSize = true;
-            this.LblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblDescripcion.Location = new System.Drawing.Point(22, 87);
-            this.LblDescripcion.Name = "LblDescripcion";
-            this.LblDescripcion.Size = new System.Drawing.Size(95, 16);
-            this.LblDescripcion.TabIndex = 8;
-            this.LblDescripcion.Text = "Descripción:";
-            this.LblDescripcion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // LblTotal
-            // 
-            this.LblTotal.AutoSize = true;
-            this.LblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblTotal.Location = new System.Drawing.Point(301, 133);
-            this.LblTotal.Name = "LblTotal";
-            this.LblTotal.Size = new System.Drawing.Size(48, 16);
-            this.LblTotal.TabIndex = 9;
-            this.LblTotal.Text = "Total:";
-            this.LblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // LblPrecioIndividual
             // 
             this.LblPrecioIndividual.AutoSize = true;
             this.LblPrecioIndividual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblPrecioIndividual.Location = new System.Drawing.Point(22, 133);
+            this.LblPrecioIndividual.Location = new System.Drawing.Point(22, 301);
             this.LblPrecioIndividual.Name = "LblPrecioIndividual";
             this.LblPrecioIndividual.Size = new System.Drawing.Size(128, 16);
             this.LblPrecioIndividual.TabIndex = 10;
@@ -121,7 +86,7 @@
             // 
             this.LblCantidad.AutoSize = true;
             this.LblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblCantidad.Location = new System.Drawing.Point(301, 92);
+            this.LblCantidad.Location = new System.Drawing.Point(22, 270);
             this.LblCantidad.Name = "LblCantidad";
             this.LblCantidad.Size = new System.Drawing.Size(74, 16);
             this.LblCantidad.TabIndex = 11;
@@ -130,40 +95,34 @@
             // 
             // TxtPrecioIndividual
             // 
-            this.TxtPrecioIndividual.Location = new System.Drawing.Point(166, 132);
+            this.TxtPrecioIndividual.Location = new System.Drawing.Point(168, 266);
             this.TxtPrecioIndividual.Name = "TxtPrecioIndividual";
             this.TxtPrecioIndividual.Size = new System.Drawing.Size(100, 20);
             this.TxtPrecioIndividual.TabIndex = 12;
             this.TxtPrecioIndividual.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // TxtTotal
-            // 
-            this.TxtTotal.Location = new System.Drawing.Point(394, 132);
-            this.TxtTotal.Name = "TxtTotal";
-            this.TxtTotal.Size = new System.Drawing.Size(100, 20);
-            this.TxtTotal.TabIndex = 13;
-            // 
             // TxtCantidad
             // 
-            this.TxtCantidad.Location = new System.Drawing.Point(394, 91);
+            this.TxtCantidad.Location = new System.Drawing.Point(168, 300);
             this.TxtCantidad.Name = "TxtCantidad";
             this.TxtCantidad.Size = new System.Drawing.Size(100, 20);
             this.TxtCantidad.TabIndex = 14;
             // 
             // BtnAgregarConsumible
             // 
-            this.BtnAgregarConsumible.Location = new System.Drawing.Point(511, 129);
+            this.BtnAgregarConsumible.Location = new System.Drawing.Point(599, 270);
             this.BtnAgregarConsumible.Name = "BtnAgregarConsumible";
-            this.BtnAgregarConsumible.Size = new System.Drawing.Size(126, 23);
+            this.BtnAgregarConsumible.Size = new System.Drawing.Size(126, 40);
             this.BtnAgregarConsumible.TabIndex = 15;
             this.BtnAgregarConsumible.Text = "Agregar consumible";
             this.BtnAgregarConsumible.UseVisualStyleBackColor = true;
+            this.BtnAgregarConsumible.Click += new System.EventHandler(this.BtnAgregarConsumible_Click);
             // 
             // BtnModificarConsumible
             // 
-            this.BtnModificarConsumible.Location = new System.Drawing.Point(643, 129);
+            this.BtnModificarConsumible.Location = new System.Drawing.Point(599, 316);
             this.BtnModificarConsumible.Name = "BtnModificarConsumible";
-            this.BtnModificarConsumible.Size = new System.Drawing.Size(126, 23);
+            this.BtnModificarConsumible.Size = new System.Drawing.Size(126, 39);
             this.BtnModificarConsumible.TabIndex = 16;
             this.BtnModificarConsumible.Text = "Modificar consumible";
             this.BtnModificarConsumible.UseVisualStyleBackColor = true;
@@ -178,7 +137,7 @@
             this.CodigoDeConsumible,
             this.Cantidad,
             this.Total});
-            this.dataGridView1.Location = new System.Drawing.Point(76, 173);
+            this.dataGridView1.Location = new System.Drawing.Point(82, 374);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(643, 150);
             this.dataGridView1.TabIndex = 17;
@@ -215,7 +174,7 @@
             // 
             // BtnGuardarCambios
             // 
-            this.BtnGuardarCambios.Location = new System.Drawing.Point(403, 343);
+            this.BtnGuardarCambios.Location = new System.Drawing.Point(411, 539);
             this.BtnGuardarCambios.Name = "BtnGuardarCambios";
             this.BtnGuardarCambios.Size = new System.Drawing.Size(126, 23);
             this.BtnGuardarCambios.TabIndex = 18;
@@ -224,31 +183,13 @@
             // 
             // BtnVolver
             // 
-            this.BtnVolver.Location = new System.Drawing.Point(271, 343);
+            this.BtnVolver.Location = new System.Drawing.Point(270, 539);
             this.BtnVolver.Name = "BtnVolver";
             this.BtnVolver.Size = new System.Drawing.Size(126, 23);
             this.BtnVolver.TabIndex = 19;
             this.BtnVolver.Text = "Volver";
             this.BtnVolver.UseVisualStyleBackColor = true;
             this.BtnVolver.Click += new System.EventHandler(this.BtnVolver_Click);
-            // 
-            // LblCodigoConsumible
-            // 
-            this.LblCodigoConsumible.AutoSize = true;
-            this.LblCodigoConsumible.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblCodigoConsumible.Location = new System.Drawing.Point(508, 92);
-            this.LblCodigoConsumible.Name = "LblCodigoConsumible";
-            this.LblCodigoConsumible.Size = new System.Drawing.Size(62, 16);
-            this.LblCodigoConsumible.TabIndex = 20;
-            this.LblCodigoConsumible.Text = "Código:";
-            this.LblCodigoConsumible.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // TxtCodigoConsumible
-            // 
-            this.TxtCodigoConsumible.Location = new System.Drawing.Point(589, 92);
-            this.TxtCodigoConsumible.Name = "TxtCodigoConsumible";
-            this.TxtCodigoConsumible.Size = new System.Drawing.Size(100, 20);
-            this.TxtCodigoConsumible.TabIndex = 21;
             // 
             // lblAllInclusive
             // 
@@ -271,33 +212,99 @@
             this.cboInclusive.Text = "Sí";
             this.cboInclusive.UseVisualStyleBackColor = true;
             // 
+            // LblCodigoEstadia
+            // 
+            this.LblCodigoEstadia.AutoSize = true;
+            this.LblCodigoEstadia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCodigoEstadia.Location = new System.Drawing.Point(296, 282);
+            this.LblCodigoEstadia.Name = "LblCodigoEstadia";
+            this.LblCodigoEstadia.Size = new System.Drawing.Size(118, 16);
+            this.LblCodigoEstadia.TabIndex = 20;
+            this.LblCodigoEstadia.Text = "Código estadía:";
+            this.LblCodigoEstadia.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(82, 90);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(643, 150);
+            this.dataGridView2.TabIndex = 24;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // txtCodigoEst
+            // 
+            this.txtCodigoEst.Location = new System.Drawing.Point(458, 281);
+            this.txtCodigoEst.Name = "txtCodigoEst";
+            this.txtCodigoEst.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoEst.TabIndex = 25;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 16);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Código consumible:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCodigoConsu
+            // 
+            this.txtCodigoConsu.Location = new System.Drawing.Point(458, 316);
+            this.txtCodigoConsu.Name = "txtCodigoConsu";
+            this.txtCodigoConsu.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoConsu.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Descripción:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(168, 335);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtDescripcion.TabIndex = 29;
+            // 
             // FrmAgregarConsumibles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 390);
+            this.ClientSize = new System.Drawing.Size(812, 587);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodigoConsu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigoEst);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.cboInclusive);
             this.Controls.Add(this.lblAllInclusive);
-            this.Controls.Add(this.TxtCodigoConsumible);
-            this.Controls.Add(this.LblCodigoConsumible);
+            this.Controls.Add(this.LblCodigoEstadia);
             this.Controls.Add(this.BtnVolver);
             this.Controls.Add(this.BtnGuardarCambios);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.BtnModificarConsumible);
             this.Controls.Add(this.BtnAgregarConsumible);
             this.Controls.Add(this.TxtCantidad);
-            this.Controls.Add(this.TxtTotal);
             this.Controls.Add(this.TxtPrecioIndividual);
             this.Controls.Add(this.LblCantidad);
             this.Controls.Add(this.LblPrecioIndividual);
-            this.Controls.Add(this.LblTotal);
-            this.Controls.Add(this.LblDescripcion);
             this.Controls.Add(this.LblRegistrarConsumibles);
-            this.Controls.Add(this.CmbDescripcion);
             this.Name = "FrmAgregarConsumibles";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmAgregarConsumibles";
+            this.Load += new System.EventHandler(this.FrmAgregarConsumibles_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -305,14 +312,10 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox CmbDescripcion;
         private System.Windows.Forms.Label LblRegistrarConsumibles;
-        private System.Windows.Forms.Label LblDescripcion;
-        private System.Windows.Forms.Label LblTotal;
         private System.Windows.Forms.Label LblPrecioIndividual;
         private System.Windows.Forms.Label LblCantidad;
         private System.Windows.Forms.TextBox TxtPrecioIndividual;
-        private System.Windows.Forms.TextBox TxtTotal;
         private System.Windows.Forms.TextBox TxtCantidad;
         private System.Windows.Forms.Button BtnAgregarConsumible;
         private System.Windows.Forms.Button BtnModificarConsumible;
@@ -325,9 +328,14 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn CodigoDeConsumible;
         private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
         private System.Windows.Forms.DataGridViewTextBoxColumn Total;
-        private System.Windows.Forms.Label LblCodigoConsumible;
-        private System.Windows.Forms.TextBox TxtCodigoConsumible;
         private System.Windows.Forms.Label lblAllInclusive;
         private System.Windows.Forms.CheckBox cboInclusive;
+        private System.Windows.Forms.Label LblCodigoEstadia;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox txtCodigoEst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigoConsu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescripcion;
     }
 }
--- FrbaHotel/RegistrarConsumible/FrmAgregarConsumibles.cs
+++ FrbaHotel/RegistrarConsumible/FrmAgregarConsumibles.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,25 @@ namespace FrbaHotel.RegistrarConsumible
 {
     public partial class FrmAgregarConsumibles : Form
     {
-        FrmSeleccionarEstadia frmSeleccionarEstadia;
+        AbmRol.frmMenuEmpleado frmMenuEmpleado;
+        SqlDataAdapter adaptador;
+        DataTable tablaConDatosConsum;
 
-        public FrmAgregarConsumibles(FrmSeleccionarEstadia form)
+
+        public FrmAgregarConsumibles(AbmRol.frmMenuEmpleado form)
         {
+            frmMenuEmpleado = form;
             InitializeComponent();
-            frmSeleccionarEstadia = form;
+        }
+
+          private void FrmAgregarConsumibles_Load(object sender, EventArgs e)
+        {
+            string consultaConsumible = "SELECT consumible_codigo,consumible_descripcion,consumible_precio FROM FAGD.Consumible";
+            adaptador = Login.FrmTipoUsuario.BD.dameDataAdapter(consultaConsumible);
+            tablaConDatosConsum = Login.FrmTipoUsuario.BD.dameDataTable(adaptador);         
+            BindingSource bSourceConsum = new BindingSource();
+            bSourceConsum.DataSource = tablaConDatosConsum;    
+            dataGridView2.DataSource = bSourceConsum;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -33,7 +47,28 @@ namespace FrbaHotel.RegistrarConsumible
         private void BtnVolver_Click(object sender, EventArgs e)
         {
             this.Close();
-            frmSeleccionarEstadia.Show();
+            frmMenuEmpleado.ShowDialog();
+           
+        }
+
+    
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dataGridView2_SelectionChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BtnAgregarConsumible_Click(object sender, EventArgs e)
+        {
+            int total = int.Parse(TxtCantidad.Text) * int.Parse(TxtPrecioIndividual.Text);
+            dataGridView1.Rows.Add(txtCodigoEst.Text,txtCodigoConsu.Text, TxtPrecioIndividual.Text, txtCodigoEst.Text, TxtCantidad.Text, total);
+        }
+
+
+      
         }
     }
-}
+
